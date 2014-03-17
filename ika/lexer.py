822a4521ce9728efbb373f8b26c1a1e69cfd6650
tioover <tioover@gmail.com>
2014-03-17 18:35:09 +0800

    lexer

--- ika/lexer.py
+++ ika/lexer.py
@@ -1,9 +1,26 @@
 import re
-
-tokens = [
+token_patterns = [
+    r"\(",
+    r"\)",
+    r"`\(",  # quote
+    r"#\(",  # vector
+    "\"[^\"]\"",  # string
+    "\'[^\']\'",  # string TODO: \"
+    r"\w+",  # name
 ]
+
+token_re = list(map(re.compile, token_patterns))  # compile patterns.
+space_re = re.compile(r"^\s+")
 
 
 def lexer(string):
     lexed = []  # result
+    while string:
+        string = space_re.sub("", string)  # remove space.
+        for re_obj in token_re:
+            match = re_obj.match(string)
+            if match:
+                lexed.append(string[:match.end()])
+                string = string[match.end():]
+                break
     return lexed
