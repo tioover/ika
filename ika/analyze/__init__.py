b56b502f313b4d01ec17f99411ed1c1ecf0380f7
tioover <tioover@gmail.com>
2014-03-19 21:11:41 +0800

    analyzer file structure

---
+++ ika/analyze/__init__.py
@@ -0,0 +1,18 @@
+from . import application, assignment, begin, cond,\
+    define, if_, lambda_, quoted, self_eval, var
+
+
+__all__ = [
+    "application",
+    "assignment",
+    "begin",
+    "cond",
+    "begin",
+    "cond",
+    "define",
+    "if_",
+    "lambda_",
+    "quoted",
+    "self_eval",
+    "var",
+]
