b2b94511246f181961b027685c584fcf7a6dfe73
tioover <tioover@gmail.com>
2014-03-25 22:53:01 +0800

    self evaluating expr analyzer

--- ika/analyze/self_evaluating.py
+++ ika/analyze/self_evaluating.py
@@ -1,6 +1,31 @@
+import re
+from ..struct import empty, t, f
+
+number_pattern = re.compile('\d+(\.\d+)?$')
+
+
+def convert_number(expr):
+    if expr.isdigit():
+        return int(expr)
+    else:
+        return float(expr)
+
+type_table = [
+    # (judgment, convert),
+    (lambda e: e[0] == e[-1] == '"', str),  # string
+    (lambda e: number_pattern.match(e), convert_number),  # number
+    (lambda e: e == "()", lambda e: empty),  # ()
+    (lambda e: e == "#t", lambda e: t),  # true
+    (lambda e: e == "#f", lambda e: f),  # false
+]
+
+
 def condition(expr):
-    pass
+    return list(filter(lambda i: i[0](expr), type_table))
 
 
 def analyze(analyzer, expr):
-    pass
+    for i in type_table:
+        if i[0](expr):
+            break
+    return i[1](expr)
