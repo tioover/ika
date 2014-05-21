fe1674f1e50d6c5e7a003560c3e7e96a4c012433
tioover <tioover@gmail.com>
2014-05-21 18:50:57 +0800

    structure

---
+++ test.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class LexerTestCase(unittest.TestCase):
+    pass
+
+
+class ParserTestCase(unittest.TestCase):
+    pass
+
+
+class InterpreterTestCase(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
